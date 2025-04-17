a2d23d416aca5a70cd9c99a33f38852c9d9fe79d
kzheng <able7252@outlook.com>
2025-04-17 16:33:42 +0000

    add dwm and some suckless software

---
+++ install/suckless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$config="$HOME/dotfiles/.config/"
+suckless=("$config/dwm" "$config/dwmblocks" 
+          "$config/dmenu" "$config/st")
+
+for suck in "${suckless[@]}"; do 
+  sudo make clean install
+  echo "Compile Success!!!"
+done
