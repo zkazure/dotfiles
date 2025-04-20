13c5ef80425198872ee139e30c259763adc85c5f
kzheng-fedora <abel7252@outlook.com>
2025-04-20 02:41:27 -0400

    增加了语法检查

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -83,14 +83,14 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +enchant) ; tasing you for misspelling mispelling
+       (spell +aspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       (debugger +lsp)         ; FIXME stepping through code, to help you add bugs
        direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
